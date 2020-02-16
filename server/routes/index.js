822ff00413e628c3d18e6d8184e086821971e4ac
张峻郡 <zhangjunjun@xinhua.org>
2020-02-16 09:11:19 +0800

    修改主页现实结果

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   // res.redirect('/index.html')
+  res.send('欢迎访问')
 });
 
 module.exports = router;
